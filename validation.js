912608dbd8707fac934c42a7d2cd418c88498104
danyellepalmore <94997942+danyellepalmore@users.noreply.github.com>
2024-10-10 12:03:17 -0400

    function for username and password regex

--- validation.js
+++ validation.js
@@ -1,26 +1,26 @@
-//when i put validation this should be the function that runs when something is wrong
-function getFieldNames(){
-   document.getElementById("output").innerHTML = "Please Enter "+document.validation.username.value;
-}
+const isValidUsername = /^[a-z0-9]{4,12}$/;
+const isValidPassword = /^(?=.*?[0-9])(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[^0-9A-Za-z]).{8,}$/;
 
-function checkUsername(){
-  const usernameInput = document.getElementById("username").value;
-  // /^ : start of input
-  // $: end of string
-  const pattern = /^[a-z0-9]$;
-  if (pattern.test(usernameInput)){
-    console.log('valid Username');
-    return true;
-  } else{
-    console.log('invalid username');
+function validateForm(){
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+
+  if (!isValidUsername.test(username)){
+    alert("Invalid username. It must be 4-12 lowercase letters or digits.");
+    return false;
+  } 
+  if (!isValidPassword.test(password)){
+    alert("Invalid password. It must be at least 8 characters long and contain at least one digit, one uppercase letter, one lowercase letter, and one special character.");
     return false;
   }
+  alert("both username and password are valid!")
+  return true;
 }
 
 function showTime() {
 	document.getElementById('currentTime').innerHTML = new Date().toUTCString();
 }
-// username: [0-9a-z]
+
 //phone number: /(\d{3})-\d{3}-\d{4}
 showTime();
 setInterval(function () {
