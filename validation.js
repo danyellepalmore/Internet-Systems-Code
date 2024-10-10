a85b0a497344a00c2bd1adcb0c2251fd78d845d2
danyellepalmore <94997942+danyellepalmore@users.noreply.github.com>
2024-10-10 13:23:04 -0400

    confusion

--- validation.js
+++ validation.js
@@ -1,28 +1,50 @@
+
 const isValidUsername = /^[a-z0-9]{4,12}$/;
-const isValidPassword = /^(?=.*?[0-9])(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[^0-9A-Za-z]).{8,}$/;
+const isValidPassword = /^[\w ]+$/;
+// /^(?=.*?[0-9])(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[^0-9A-Za-z]).{8,}$/;
 
 function validateForm(){
-  const username = document.getElementById("username").value;
-  const password = document.getElementById("password").value;
+//   let isValid = true;
+//   // const element = document.getElementById("error");
+//   // document.getElementById("other").innerHTML = element.innerHTML;
+  
+//   const username = document.getElementById("username");
+//   const errorMessage = document.getElementById("error-message"); 
 
-  if (!isValidUsername.test(username)){
-    alert("Invalid username. It must be 4-12 lowercase letters or digits.");
-    return false;
-  } 
-  if (!isValidPassword.test(password)){
-    alert("Invalid password. It must be at least 8 characters long and contain at least one digit, one uppercase letter, one lowercase letter, and one special character.");
-    return false;
-  }
-  alert("both username and password are valid!")
-  return true;
-}
+//     if (username.value === "") {
+//       usernameLabel.classList.add("error-label");
+//       errorMessage.innerHTML = 'username';
+//       isValid = false;
+//     } else {
+//       errorMessage.textContent = "";
+//     }
+//     return isValid;
+// }
+// function handleSubmit(){
+//   if (validateForm()){
+//     document.getElementById("form").submit();
+//   }
+// }
+  
+  
+  
+//   const password = document.getElementById("password").value;
 
-function showTime() {
-	document.getElementById('currentTime').innerHTML = new Date().toUTCString();
-}
+  // if (!isValidUsername.test(username)){
+  //   document.write("Invalid username. It must be 4-12 lowercase letters or digits.");
+  //   validation.username.focus();
+  //   return false;
+  // } 
+//   if (!isValidPassword.test(password)){
+//     alert("Invalid password. It must be at least 8 characters long and contain at least one digit, one uppercase letter, one lowercase letter, and one special character.");
+//     form.password.focus();
+//     return false;
+//   }
+//   alert("both username and password are valid!")
+//   return true;
+// }
+
+
+
 
 //phone number: /(\d{3})-\d{3}-\d{4}
-showTime();
-setInterval(function () {
-	showTime();
-}, 1000);
